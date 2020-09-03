36f3aaf2409c3a512fb399dc7c848760e91df575
sabaja <jacoposabatini76@gmail.com>
2020-09-03 15:42:39 +0200

    Add Serialazable to User.java

--- src/main/java/com/rest/optional/stream/api/bin/User.java
+++ src/main/java/com/rest/optional/stream/api/bin/User.java
@@ -1,18 +1,20 @@
 package com.rest.optional.stream.api.bin;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 
-@Builder
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class User {
+public class User implements Serializable {
+
+    private static final long serialVersionUID = -3413836556179887134L;
     private BigInteger id;
     private String name;
     private String email;
@@ -20,5 +22,4 @@ public class User {
     private String status;
     private LocalDateTime created_at;
     private LocalDateTime updated_at;
-
 }
